d9923db00374a4781fb5d34f059ae43ed2ff1453
DeadbraiN <deadbrainman@gmail.com>
2019-01-13 00:01:00 +0200

    added unit tests for Bytes2Code #4

---
+++ src/irma/Bytes2Code.spec.js
@@ -0,0 +1,21 @@
+describe('src/irma/Bytes2Code', () => {
+    const Bytes2Code    = require('./Bytes2Code');
+    const Config        = require('./../Config');
+    const CODE_CMD_OFFS = Config.CODE_CMD_OFFS;
+
+    it('Checks constant number command', () => {
+        expect(Bytes2Code.toCode([CODE_CMD_OFFS - 1])).toBe(`\n${CODE_CMD_OFFS - 1}`);
+        expect(Bytes2Code.toCode([CODE_CMD_OFFS - 2])).toBe(`\n${CODE_CMD_OFFS - 2}`);
+        expect(Bytes2Code.toCode([0])).toBe(`\n${0}`);
+        expect(Bytes2Code.toCode([-1])).toBe(`\n${-1}`);
+        expect(Bytes2Code.toCode([-CODE_CMD_OFFS])).toBe(`\n${-CODE_CMD_OFFS}`);
+    });
+
+    it('Checks non constant commands', () => {
+        for (let i = 0, len = Object.keys(Bytes2Code.MAP).length; i < len; i++) {
+            expect(Bytes2Code.toCode([CODE_CMD_OFFS + i])).toBe(`\n${Bytes2Code.MAP[CODE_CMD_OFFS + i]}`);
+            expect(Bytes2Code.toCode([CODE_CMD_OFFS + i, CODE_CMD_OFFS + i])).toBe(`\n${Bytes2Code.MAP[CODE_CMD_OFFS + i]}\n${Bytes2Code.MAP[CODE_CMD_OFFS + i]}`);
+        }
+
+    });
+});
