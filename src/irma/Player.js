ab6d722c9f64c32741ed45dfe1451741b3a29605
Stanislav Chechotkin <schechot@softservecom.com>
2019-12-11 11:23:31 +0200

    Ticket #59. Fix PR issues.

--- src/irma/Player.js
+++ src/irma/Player.js
@@ -5,137 +5,74 @@
  */
 const Helper = require('./../common/Helper');
 const STATION_TITLE_PREFIX = 'Now playing: ';
-const LOCAL_STORAGE_KEY = 'IRMA_current_station_index';
+const LOCAL_STORAGE_KEY = 'irma-index';
 
 class Player {
     constructor(options) {
         this._stations = options.stations;
-        this._stationIndex = localStorage.getItem(LOCAL_STORAGE_KEY) || 0;
+        this._stationIndex = localStorage[LOCAL_STORAGE_KEY] || 0;
         
-        this._audioEl = this._createAudioElement();
-        this._playBtn = this._createPlayButton();
-        this._nextBtn = this._createNextButton();
-        this._titleEl = this._createStationTitle();
+        this._rootEl = this._createHtmlElements();
     }
 
     destroy() {
         const parentNode = document.body;
 
-        parentNode.removeChild(this._audioEl);
-        parentNode.removeChild(this._playBtn);
-        parentNode.removeChild(this._nextBtn);
-        parentNode.removeChild(this._titleEl);
+        parentNode.removeChild(this._rootEl);
+        this._stations = null;
+        this._stationIndex = null;
+        this._rootEl = null;
+    }
 
-        this._audioEl = null;
-        this._playBtn = null;
-        this._nextBtn = null;
-        this._titleEl = null;
-     }
-
-    _createAudioElement() {
-        const audio = document.createElement('audio');
-        audio.setAttribute('src', this._stations[this._stationIndex].source);
-        audio.setAttribute('preload', 'none');
-        audio.pause();
+    _createHtmlElements() {
+        const player = document.createElement('div');
+        player.setAttribute('id', 'player');
+        player.innerHTML = `
+            <audio id="audio" src="${this._stations[this._stationIndex].source}" preload="none"></audio>
+            <div id="play" title="Play Music" style="position: absolute; width: 20px; height: 20px; top: 7px; right: 34px; border: 1px solid rgb(0, 0, 0); background-size: 8px 8px; border-radius: 6px; background-color: rgb(255, 235, 59); cursor: pointer; text-align: center;">▷</div>
+            <div id="next" title="Next Station" style="position: absolute; width: 20px; height: 20px; top: 7px; right: 9px; border: 1px solid rgb(0, 0, 0); background-size: 8px 8px; border-radius: 6px; background-color: rgb(255, 235, 59); cursor: pointer; text-align: center;">»</div>
+        `;
+        const el = document.body.appendChild(player);
         
-        /**
-         * Trigger play event when stream is loaded.
-         * This behaviour avoids Exception in interrupted Play promise 
-         */ 
-        audio.addEventListener('canplay', () => {
-            audio.play();
+        el.querySelector('#play').addEventListener('click', this._onPlayButton.bind(this), true);
+        el.querySelector('#next').addEventListener('click', this._onNextButton.bind(this), true);
+        el.querySelector('#audio').addEventListener('canplay', () => {
+            el.querySelector('#audio').play();
         }, false);
 
-        return document.body.appendChild(audio);
-    }
-
-    _createPlayButton() {
-        const el = document.body.appendChild(Helper.setStyles('DIV', {
-            position: 'absolute',
-            width: '20px',
-            height: '20px',
-            top: '7px',
-            right: '34px',
-            border: '1px #FFEB3B solid',
-            backgroundSize: '8px 8px',
-            borderRadius: '6px',
-            backgroundColor: '#FFEB3B',
-            cursor: 'pointer',
-            textAlign: 'center'
-        }));
-
-        el.innerText = '▷';
-        el.title = 'Play Music';
-        el.addEventListener('click', this._onPlayButton.bind(this), true);
-
         return el;
-    }
-
-    _createNextButton() {
-        const el = document.body.appendChild(Helper.setStyles('DIV', {
-            position: 'absolute',
-            width: '20px',
-            height: '20px',
-            top: '7px',
-            right: '9px',
-            border: '1px #FFEB3B solid',
-            backgroundSize: '8px 8px',
-            borderRadius: '6px',
-            backgroundColor: '#FFEB3B',
-            cursor: 'pointer',
-            textAlign: 'center'
-        }));
-
-        el.innerText = '»';
-        el.title = 'Next Station';
-        el.addEventListener('click', this._onNextButton.bind(this), true);
-
-        return el;
-    }
-
-    _createStationTitle() {
-        return document.body.appendChild(Helper.setStyles('DIV', {
-            position: 'absolute',
-            top: '35px',
-            left: '60px',
-            color: '#fff',
-            fontSize: '18px',
-            fontFamily: 'Consolas'
-        }));
     }
 
     _onPlayButton() {
-        if (this._audioEl.paused) {
-            this._playBtn.innerHTML = '||';
-            this._audioEl.load();
-            
-            this._playBtn.setAttribute('title', STATION_TITLE_PREFIX + this._stations[this._stationIndex].title);
-            this._audioEl.setAttribute('preload', 'auto');
-        } else {
-            this._audioEl.pause();
-            this._playBtn.innerHTML = '▷';
-            this._playBtn.setAttribute('title', this._stations[this._stationIndex].title);
-            
-            this._audioEl.setAttribute('preload', 'none');
-        }
+        const audioEl = this._rootEl.querySelector('#audio');
+        const playBtn  = this._rootEl.querySelector('#play');
 
+        if (audioEl.paused) {
+            playBtn.innerHTML = '||';
+            audioEl.load();
+            
+            playBtn.setAttribute('title', STATION_TITLE_PREFIX + this._stations[this._stationIndex].title);
+            audioEl.setAttribute('preload', 'auto');
+        } else {
+            audioEl.pause();
+            playBtn.innerHTML = '▷';
+            playBtn.setAttribute('title', this._stations[this._stationIndex].title);
+            
+            audioEl.setAttribute('preload', 'none');
+        }
     }
 
     _onNextButton() {
+        const audioEl = this._rootEl.querySelector('#audio');
+        const playBtn  = this._rootEl.querySelector('#play');
+
         const station = this._pickStation();
-        this._audioEl.setAttribute('src', station.source);
+
+        audioEl.setAttribute('src', station.source);
 
         // Title without prefix
-        if (this._audioEl.paused) {
-            this._playBtn.setAttribute('title', this._stations[this._stationIndex].title);
-        }
-    }
-
-    _updateTitle(text = '') {
-        this._titleEl.innerHTML = text;
-        // self-clean
-        if (text != '') {
-            setTimeout(() => this._updateTitle(), 5000);
+        if (audioEl.paused) {
+            playBtn.setAttribute('title', this._stations[this._stationIndex].title);
         }
     }
 
@@ -145,7 +82,7 @@ class Player {
         } else {
             this._stationIndex++;
         }
-        localStorage.setItem(LOCAL_STORAGE_KEY, this._stationIndex);
+        localStorage[LOCAL_STORAGE_KEY] = this._stationIndex;
 
         return this._stations[this._stationIndex];
     }
