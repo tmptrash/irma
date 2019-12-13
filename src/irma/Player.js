78378c6ce6be28aa5cfa4831cf624e2aa924b358
Stanislav Chechotkin <schechot@softservecom.com>
2019-12-13 15:26:51 +0200

    Ticket #59. Fix PR issues.

--- src/irma/Player.js
+++ src/irma/Player.js
@@ -3,88 +3,83 @@
  *
  * @author zostrum
  */
-const Helper = require('./../common/Helper');
 const STATION_TITLE_PREFIX = 'Now playing: ';
-const LOCAL_STORAGE_KEY = 'irma-index';
+const STATION = 'irma-station';
 
 class Player {
     constructor(options) {
         this._stations = options.stations;
-        this._stationIndex = localStorage[LOCAL_STORAGE_KEY] || 0;
+        this._stationIndex = localStorage[STATION] || 0;
         
-        this._rootEl = this._createHtmlElements();
+        this._rootEl = this._createDOM();
     }
 
     destroy() {
-        const parentNode = document.body;
-
-        parentNode.removeChild(this._rootEl);
+        document.body.removeChild(this._rootEl);
         this._stations = null;
-        this._stationIndex = null;
         this._rootEl = null;
     }
 
-    _createHtmlElements() {
-        const player = document.createElement('div');
-        player.setAttribute('id', 'player');
-        player.innerHTML = `
-            <audio id="audio" src="${this._stations[this._stationIndex].source}" preload="none"></audio>
-            <div id="play" title="Play Music" style="position: absolute; width: 20px; height: 20px; top: 7px; right: 34px; border: 1px solid rgb(0, 0, 0); background-size: 8px 8px; border-radius: 6px; background-color: rgb(255, 235, 59); cursor: pointer; text-align: center;">▷</div>
-            <div id="next" title="Next Station" style="position: absolute; width: 20px; height: 20px; top: 7px; right: 9px; border: 1px solid rgb(0, 0, 0); background-size: 8px 8px; border-radius: 6px; background-color: rgb(255, 235, 59); cursor: pointer; text-align: center;">»</div>
+    _createDOM() {  
+        const playerEl = document.createElement('div');
+        playerEl.id = 'player';
+        playerEl.innerHTML = `
+            <style>
+                .play, .next {
+                    position: absolute;
+                    width: 20px; 
+                    height: 20px; 
+                    top: 7px; 
+                    border: 1px solid rgb(0, 0, 0); 
+                    background-size: 8px 8px; 
+                    border-radius: 6px; 
+                    background-color: rgb(255, 235, 59); 
+                    cursor: pointer; 
+                    text-align: center;
+                }
+            </style>
+            <audio class="audio" src="${this._stations[this._stationIndex].source}" preload="none"></audio>
+            <div class="play" class="button" title="Play Music"   style="right: 34px;">▶</div>
+            <div class="next" class="button" title="Next Station" style="right: 9px;">»</div>
         `;
-        const el = document.body.appendChild(player);
+        document.body.appendChild(playerEl);
         
-        el.querySelector('#play').addEventListener('click', this._onPlayButton.bind(this), true);
-        el.querySelector('#next').addEventListener('click', this._onNextButton.bind(this), true);
-        el.querySelector('#audio').addEventListener('canplay', () => {
-            el.querySelector('#audio').play();
-        }, false);
+        playerEl.querySelector('.play').addEventListener('click', this._onPlayButton.bind(this), true);
+        playerEl.querySelector('.next').addEventListener('click', this._onNextButton.bind(this), true);
+        playerEl.querySelector('.audio').addEventListener('canplay', () => playerEl.querySelector('.audio').play(), false);
 
-        return el;
+        return playerEl;
     }
 
     _onPlayButton() {
-        const audioEl = this._rootEl.querySelector('#audio');
-        const playBtn  = this._rootEl.querySelector('#play');
+        event.stopPropagation();
+        const audioEl = this._rootEl.querySelector('.audio');
+        const playBtn  = this._rootEl.querySelector('.play');
 
         if (audioEl.paused) {
-            playBtn.innerHTML = '||';
             audioEl.load();
-            
-            playBtn.setAttribute('title', STATION_TITLE_PREFIX + this._stations[this._stationIndex].title);
-            audioEl.setAttribute('preload', 'auto');
+            playBtn.innerText = '||';
+            playBtn.title = STATION_TITLE_PREFIX + this._stations[this._stationIndex].title;
+            audioEl.preload = 'auto';
         } else {
             audioEl.pause();
-            playBtn.innerHTML = '▷';
-            playBtn.setAttribute('title', this._stations[this._stationIndex].title);
-            
-            audioEl.setAttribute('preload', 'none');
+            playBtn.innerText = '▶';
+            playBtn.title = this._stations[this._stationIndex].title;
+            audioEl.preload = 'none';
         }
     }
 
     _onNextButton() {
-        const audioEl = this._rootEl.querySelector('#audio');
-        const playBtn  = this._rootEl.querySelector('#play');
-
-        const station = this._pickStation();
-
-        audioEl.setAttribute('src', station.source);
+        const audioEl = this._rootEl.querySelector('.audio');
+        const playBtn  = this._rootEl.querySelector('.play');
+        const station = this._stations[++this._stationIndex] || this._stations[this._stationIndex = 0];
+        localStorage[STATION] = this._stationIndex;
+        audioEl.src = station.source;
 
         // Title without prefix
         if (audioEl.paused) {
-            playBtn.setAttribute('title', this._stations[this._stationIndex].title);
+            playBtn.title = this._stations[this._stationIndex].title;
         }
-    }
-
-    _pickStation() {
-        if (this._stationIndex == this._stations.length - 1) {
-            this._stationIndex = 0;
-        } else {
-            this._stationIndex++;
-        }
-        localStorage[LOCAL_STORAGE_KEY] = this._stationIndex;
-
-        return this._stations[this._stationIndex];
     }
 }
 
