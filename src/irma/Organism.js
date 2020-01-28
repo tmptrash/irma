d03c6cb20f53f748016e565924ff574ea30ff4e3
DeadbraiN <deadbrainman@gmail.com>
2020-01-28 22:34:48 +0200

    fixed recompilation issue

--- src/irma/Organism.js
+++ src/irma/Organism.js
@@ -131,8 +131,8 @@ class Organism {
         // Updates current line
         //
         const line   = this.line;
-        if (line >= index1) {this.line += amount}
-        else if (line >= index1 && line <= index2) {this.line = index1}
+        if (line >= index2) {this.line += amount}
+        else if (line >= index1 && line < index2) {this.line = index1}
         //
         // Updates function metadata (indexes in a code). If amount of functions
         // were changed we have to remove call stack. In other case we have to 
